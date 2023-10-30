05f076b60fa3a4e46d4816c2e6d333ffc70698da
aakib02 <146342937+aakib02@users.noreply.github.com>
2023-10-30 08:53:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import Cart from './Cart'
-import Copy from './Copy'
-// import State from './Stat'
-// import Navbar from './Navbar'
-// import { Data1 } from './Data1'
-// import State from './Stat'
-// import State1 from './Stat1'
-
 const App = ()=>{
     return(
         <div>
-          
          <Cart/>
-
-
-           
         </div>
     )
 }
